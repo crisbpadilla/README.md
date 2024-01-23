63d5b9d461b80359e0cb69c7b5a9b6443a83ed17
crisbpadilla <157447771+crisbpadilla@users.noreply.github.com>
2024-01-22 21:14:13 -0800

    Create firstpython.py

---
+++ firstpython.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+data={'Month':['Jan','Feb','March','Apr','Jun'],'income':[1200,3000,4000,1200,2000]}
+df=pd.DataFrame(data)
+df
+print('my first data frame with python code in this rep')
